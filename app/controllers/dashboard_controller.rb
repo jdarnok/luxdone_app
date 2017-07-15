73c9a7e6079d681e00bca24721af7b7728b54fa4
Konrad Janczyk <janczyk.konrad@gmail.com>
2017-07-15 14:47:17 +0200

    adding dashboard_controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,32 @@
 class DashboardController < ApplicationController
+  before_action :set_client, only: [:twitter_search, :show_country_tweets]
   def index
-
   end
+
+  def twitter_search
+    search = params[:search][:hash].gsub('#','')
+    search = search.prepend("#")
+    countries = Country.get_eu_countries
+    @tweets_array = Country.search_tweets_in_countries(countries, @client, search)
+    @tweets_array
+  end
+
+  def show_country_tweets
+    search = params[:search].prepend("#")
+    country = Country.find_by(name: params[:country])
+    @tweets = @client.search("place:#{country.twitter_id} #{search}").to_a.collect{|tweet| [tweet.user.name, tweet.text] }
+  end
+
+  private
+
+  def set_client
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key        =  ENV['CONSUMER_KEY']
+      config.consumer_secret     =  ENV['CONSUMER_SECRET']
+      config.access_token        =  ENV['ACCESS_TOKEN']
+      config.access_token_secret =  ENV['ACCESS_TOKEN_SECRET']
+    end
+    @client
+  end
+
 end
