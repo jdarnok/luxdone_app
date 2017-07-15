387882e4fbe4fc36dab84bbb515c820199fa1ffc
Konrad Janczyk <janczyk.konrad@gmail.com>
2017-07-15 14:47:46 +0200

    country model, dashboard index view

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,2 +1,24 @@
 class Country < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+  validates :twitter_id, presence: true, uniqueness: true
+
+  def self.get_eu_countries
+    eu_countries = []
+    ISO3166::Country.find_all_countries_by_region('europe').each do |country|
+      eu_countries << country.name unless country.nil?
+    end
+    countries = Country.where(name: eu_countries)
+    countries
+  end
+
+  def self.search_tweets_in_countries(countries, client, search)
+    tweets_array = []
+    countries.each do |country|
+      tweets = client.search("place:#{country.twitter_id} #{search}").to_a
+      tweets_content = tweets.each.collect{|t| [t.user.name, t.text] }
+      tweets_array << {country: country.name, tweets_size: tweets.size, tweets: tweets_content }
+    end
+    tweets_array = tweets_array.collect { |k| [k[:country], k[:tweets_size]] }
+    tweets_array
+  end
 end
---
+++ app/views/dashboard/twitter_search.coffee
@@ -0,0 +1,17 @@
+countries_table = <%= raw @tweets_array.to_json %>
+console.log countries_table
+chart = new (google.visualization.GeoChart)(document.getElementById('geo-chart-container'))
+data = google.visualization.arrayToDataTable([
+  [
+    'Country'
+    'Tweets'
+  ]
+  <%= raw @tweets_array.to_s.gsub("[[","[").gsub("]]", "]") %>
+])
+chart.draw data
+google.visualization.events.addListener chart, 'select', ->
+  selectedItem = chart.getSelection()[0]
+  country = data.getValue(selectedItem.row, 0)
+  search = $('#search_hash')[0].value.replace('#','')
+  window.location.href = window.location.href + 'tweets/' + country + '?search=' + search
+  console.log country
