46f3002c8b5d169f5e2529c84b4355b394dbe6ae
Konrad Janczyk <janczyk.konrad@gmail.com>
2017-07-15 14:48:44 +0200

    adding schema

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170709142430) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "countries", force: :cascade do |t|
     t.string "twitter_id"
     t.string "name"
