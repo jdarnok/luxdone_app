354e4cc01e3ff31d29b4deca17dd9350cd33974a
Konrad Janczyk <janczyk.konrad@gmail.com>
2017-07-15 14:48:10 +0200

    adding seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-countries = ISO3166::Country.find_all_countries_by_region('Europe').collect { |c| c.name }
+countries = ISO3166::Country.all.collect { |c| c.name }
 countries_sliced = countries.each_slice(4).to_a
 client = Twitter::REST::Client.new do |config|
-  config.consumer_key        =  ENV['CONSUMER_KEY']
-  config.consumer_secret     =  ENV['CONSUMER_SECRET']
-  config.access_token        =  ENV['ACCESS_TOKEN']
-  config.access_token_secret =  ENV['ACCESS_TOKEN_SECRET']
+  config.consumer_key = ENV['CONSUMER_KEY']
+  config.consumer_secret = ENV['CONSUMER_SECRET']
+  config.access_token = ENV['ACCESS_TOKEN']
+  config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
 end
 countries_sliced.each do |sliced_array|
   sliced_array.each do |country|
     puts "adding #{country}\n"
     begin
-      Country.create(twitter_id: client.geo_search(query: country).first.id, name: country)
+      begin
+        Country.create(twitter_id: client.geo_search(query: country).first.id, name: country)
+      rescue NoMethodError
+        next
+      end
     rescue Twitter::Error::TooManyRequests
       sleep 901
     end
